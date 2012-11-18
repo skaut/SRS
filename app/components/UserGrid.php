9c72690a7ad0488b5998364be1de63d78d51cee9
Michal Majsky <m.majsky@gmail.com>
2012-11-18 18:03:29 +0100

    +

--- app/components/UserGrid.php
+++ app/components/UserGrid.php
@@ -8,6 +8,7 @@
  */
 namespace SRS\Components;
 use \NiftyGrid\Grid;
+use \Doctrine\ORM\Query\Expr;
 
 /**
  * Grid pro správu uživatelů a práv
@@ -20,7 +21,7 @@ class UserGrid extends Grid
     protected $em;
 
     /**
-     * @param \Doctrine\ORM\EntityManager
+     * @param \Nette\ComponentModel\IContainer $em
      */
     public function __construct($em)
     {
@@ -30,14 +31,43 @@ class UserGrid extends Grid
 
     protected function configure($presenter)
     {
-        $source = new \NiftyGrid\DataSource\DoctrineDataSource($this->em->createQueryBuilder()->add('select', 'u')->add('from', '\SRS\Model\User u'), 'u_id');
+        $qb = $this->em->createQueryBuilder();
+        $qb->addSelect('u');
+        $qb->addSelect('role');
+        $qb->from('\SRS\Model\User', 'u');
+        $qb->leftJoin('u.roles','role'); //'WITH', 'role.standAlone=1');
+        $query = $qb->getQuery();
+        $users = $query->getResult();
+        \Nette\Diagnostics\Debugger::dump($qb->getQuery()->getScalarResult());
+        //\Nette\Diagnostics\Debugger::dump($users);
+
+        $roles = $this->em->getRepository('\SRS\Model\Role')->findAll();
+        $rolesGrid = array();
+
+        foreach ($roles as $role) {
+            $rolesGrid[$role->id] = $role->name;
+        }
+        $source = new \NiftyGrid\DataSource\DoctrineDataSource($qb, 'u_id');
+
         $this->setDataSource($source);
         $numOfResults = 10;
         $this->addColumn('u_username', 'Username')->setTextFilter()->setAutocomplete($numOfResults);
         $this->addColumn('u_nickName', 'Přezdívka')->setTextFilter()->setAutocomplete($numOfResults);
         $this->addColumn('u_firstName', 'Jméno')->setTextFilter()->setAutocomplete($numOfResults);
         $this->addColumn('u_lastName', 'Příjmení')->setTextFilter()->setAutocomplete($numOfResults);
+        $this->addColumn('role_id', 'Role')
+            ->setRenderer(function($row) {
+                return $row->role_name;
+            })
+            ->setSelectFilter($rolesGrid)
+            ->setSelectEditable($rolesGrid);
 
+        $this->setRowFormCallback(function($values) {
+            \Nette\Diagnostics\Debugger::dump($values);
+        });
+
+        $this->addButton(Grid::ROW_FORM, "Změnit")
+            ->setClass("fast-edit");
     }
 
 }
