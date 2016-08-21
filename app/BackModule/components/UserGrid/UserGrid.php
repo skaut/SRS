1f35a36822e784b22818973a70e4efe5b5e61f5b
jan-stanek <jan@jstanek.cz>
2016-08-21 23:15:56 +0200

    omezení kapacity rolí

--- app/BackModule/components/UserGrid/UserGrid.php
+++ app/BackModule/components/UserGrid/UserGrid.php
@@ -46,7 +46,8 @@ class UserGrid extends Grid
 
         foreach ($roles as $role) {
             if ($role->name != Role::GUEST) {
-            $rolesGrid[$role->id] = $role->name;
+                if ($role->usersLimit == null || count($role->users) < $role->usersLimit)
+                    $rolesGrid[$role->id] = $role->name;
             }
         }
         $source = new \SRS\SRSDoctrineDataSource($qb, 'id');
@@ -63,7 +64,6 @@ class UserGrid extends Grid
             return $row->role['name'];
         })
             ->setSelectFilter($rolesGrid)
-
             ->setSelectEditable($rolesGrid);
         $this->addColumn('approved', 'Schválený')->setBooleanFilter()->setBooleanEditable()
             ->setRenderer(function ($row) {
@@ -79,14 +79,15 @@ class UserGrid extends Grid
         $this->setRowFormCallback(function ($values) use ($self, $presenter) {
                 $userToSave = $presenter->context->database->getRepository('\SRS\Model\User')->find($values['id']);
                 $newRole = $presenter->context->database->getRepository('SRS\Model\Acl\Role')->find($values['role']);
+
                 if ($newRole != null) {
                     $userToSave->role = $newRole;
                 }
+
                 $userToSave->approved = isset($values['approved']) ? 1 : 0;
                 $presenter->context->database->flush();
 
                 $self->flashMessage("Záznam byl úspěšně uložen.", "success");
-
             }
         );
 
