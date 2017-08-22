a130fbeff2969c2621c6615c5bda5a643faaaa40
Jan Staněk <jan@jstanek.cz>
2017-08-22 18:32:43 +0200

    editace šablony automatického mailu

--- app/AdminModule/MailingModule/presenters/TemplatesPresenter.php
+++ app/AdminModule/MailingModule/presenters/TemplatesPresenter.php
@@ -3,7 +3,9 @@
 namespace App\AdminModule\MailingModule\Presenters;
 
 use App\AdminModule\MailingModule\Components\IMailTemplatesGridControlFactory;
+use App\AdminModule\MailingModule\Forms\EditTemplateForm;
 use App\Model\Mailing\TemplateRepository;
+use Nette\Forms\Form;
 
 
 /**
@@ -25,6 +27,12 @@ class TemplatesPresenter extends MailingBasePresenter
      */
     public $mailTemplatesGridControlFactory;
 
+    /**
+     * @var EditTemplateForm
+     * @inject
+     */
+    public $editTemplateFormFactory;
+
 
     /**
      * @param $id
@@ -34,10 +42,33 @@ class TemplatesPresenter extends MailingBasePresenter
         $template = $this->templateRepository->findById($id);
 
         $this->template->editedTemplate = $template;
+        $this->template->translator = $this->translator;
     }
 
+    /**
+     * @return \App\AdminModule\MailingModule\Components\MailTemplatesGridControl
+     */
     protected function createComponentMailTemplatesGrid()
     {
         return $this->mailTemplatesGridControlFactory->create();
+    }
+
+    /**
+     * @return \Nette\Application\UI\Form
+     */
+    protected function createComponentEditTemplateForm()
+    {
+        $form = $this->editTemplateFormFactory->create($this->getParameter('id'));
+
+        $form->onSuccess[] = function (Form $form, \stdClass $values) {
+            if ($form['cancel']->isSubmittedBy())
+                $this->redirect('Templates:default');
+
+            $this->flashMessage('admin.mailing.templates_saved', 'success');
+
+            $this->redirect('Templates:default');
+        };
+
+        return $form;
     }
 }
