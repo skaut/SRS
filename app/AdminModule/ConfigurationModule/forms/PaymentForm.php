a1122f06bd08796219b6467ab4f3e35d992c99b0
Jan Staněk <stanej16@fit.cvut.cz>
2018-10-11 01:35:02 +0200

    nastaveni v konfiguraci

--- app/AdminModule/ConfigurationModule/forms/PaymentForm.php
+++ app/AdminModule/ConfigurationModule/forms/PaymentForm.php
@@ -80,6 +80,8 @@ class PaymentForm extends UI\Control
             ->addCondition(Form::FILLED)
             ->addRule(Form::PATTERN, 'admin.configuration.variable_symbol_code_format', '^\d{0,4}$');
 
+        $form->addText('accountToken', 'admin.configuration.account_token');
+
         $maturityTypeSelect = $form->addSelect('maturityType', 'admin.configuration.maturity_type', $this->prepareMaturityTypeOptions());
         $maturityTypeSelect->addCondition($form::EQUAL, MaturityType::DATE)
             ->toggle('maturity-date')
@@ -178,6 +180,10 @@ class PaymentForm extends UI\Control
                 Settings::CANCEL_REGISTRATION_AFTER_MATURITY,
                 $values['cancelRegistrationAfterMaturity'] !== '' ? $values['cancelRegistrationAfterMaturity'] : null
             );
+        }
+
+        if ($values['accountToken'] !== "") {
+            $this->settingsRepository->setValue(Settings::ACCOUNT_TOKEN, $values['accountToken']);
         }
 
         $this->onSave($this);
