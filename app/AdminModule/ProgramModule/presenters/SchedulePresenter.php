9beab21d71d83304f58faf0b2d59e6398b482c03
Jan Staněk <jan@jstanek.cz>
2019-02-16 10:16:14 +0100

    #510, #522 - pridani hlasek

--- app/AdminModule/ProgramModule/presenters/SchedulePresenter.php
+++ app/AdminModule/ProgramModule/presenters/SchedulePresenter.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\AdminModule\ProgramModule\Presenters;
 
+use App\Model\Settings\Settings;
+
 /**
  * Presenter obsluhující správu harmonogramu.
  *
@@ -12,8 +14,13 @@ namespace App\AdminModule\ProgramModule\Presenters;
  */
 class SchedulePresenter extends ProgramBasePresenter
 {
+    /**
+     * @throws \App\Model\Settings\SettingsException
+     * @throws \Throwable
+     */
     public function renderDefault() : void
     {
         $this->template->containerAttributes = 'ng-app="scheduleApp" ng-controller="AdminScheduleCtrl"';
+        $this->template->isAllowedModifySchedule = $this->settingsRepository->getBoolValue(Settings::IS_ALLOWED_MODIFY_SCHEDULE);
     }
 }
