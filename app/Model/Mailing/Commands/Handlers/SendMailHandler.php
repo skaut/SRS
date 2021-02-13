0ba0cc1bd15413d5c1862f39413db3f1ed5bc161
Jan Staněk <jan@jstanek.cz>
2021-02-13 11:57:02 +0100

    commands

---
+++ app/Model/Mailing/Commands/Handlers/SendMailHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Program\Commands\Handlers;
+
+use App\Model\Program\Commands\RemoveBlock;
+use App\Model\Program\Commands\RemoveProgram;
+use App\Model\Program\Commands\SendMail;
+use App\Model\Program\Repositories\BlockRepository;
+use App\Services\CommandBus;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class SendMailHandler implements MessageHandlerInterface
+{
+    private CommandBus $commandBus;
+
+    private EntityManagerInterface $em;
+
+    private BlockRepository $blockRepository;
+
+    public function __construct(CommandBus $commandBus, EntityManagerInterface $em, BlockRepository $blockRepository)
+    {
+        $this->commandBus      = $commandBus;
+        $this->em              = $em;
+        $this->blockRepository = $blockRepository;
+    }
+
+    public function __invoke(SendMail $command): void
+    {
+        $this->em->transactional(function () use ($command): void {
+            $block = $command->getBlock();
+
+            foreach ($block->getPrograms() as $program) {
+                $this->commandBus->handle(new RemoveProgram($program));
+            }
+
+            $this->blockRepository->remove($block);
+        });
+    }
+}
---
+++ app/Model/Mailing/Commands/SendQueue.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Program\Commands;
+
+use App\Model\Program\Block;
+
+class SendQueue
+{
+    private Block $block;
+
+    public function __construct(Block $block)
+    {
+        $this->block = $block;
+    }
+
+    public function getBlock(): Block
+    {
+        return $this->block;
+    }
+}
