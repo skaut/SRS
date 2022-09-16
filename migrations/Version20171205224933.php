f29702e3d0c0f8d9ec829b98a6a5e904bff3f1ba
Jan Staněk <jan@jstanek.cz>
2022-09-16 22:28:00 +0200

    utf8mb4_bin

--- migrations/Version20171205224933.php
+++ migrations/Version20171205224933.php
@@ -16,7 +16,7 @@ class Version20171205224933 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('ALTER TABLE user CHANGE username username VARCHAR(255) DEFAULT NULL COLLATE utf8_bin');
+        $this->addSql('ALTER TABLE user CHANGE username username VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_bin');
     }
 
     public function down(Schema $schema): void
