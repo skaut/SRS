c00dbe2153d178d616b05d505aa4ff909e1baf45
Jan Staněk <jan@jstanek.cz>
2022-09-16 22:31:23 +0200

    utf8mb4_bin

--- migrations/Version20220916203045.php
+++ migrations/Version20220916203045.php
@@ -10,16 +10,20 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-
-class Version20171205224933 extends AbstractMigration
+final class Version20220916203045 extends AbstractMigration
 {
-    public function up(Schema $schema): void
+    public function getDescription() : string
     {
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('ALTER TABLE user CHANGE username username VARCHAR(255) DEFAULT NULL COLLATE utf8_bin');
+        return '';
     }
 
-    public function down(Schema $schema): void
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('ALTER TABLE user CHANGE username username VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_bin');
+    }
+
+    public function down(Schema $schema) : void
     {
     }
 }
