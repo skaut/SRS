578b5684c6ea131c9d3b86ea83ddcb6be296e9fc
Jan Staněk <jan@jstanek.cz>
2022-09-16 22:47:54 +0200

    cs fix

--- migrations/Version20220916203759.php
+++ migrations/Version20220916203759.php
@@ -12,19 +12,19 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20220916203759 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE user CHANGE username username VARCHAR(255) DEFAULT NULL COLLATE `utf8mb4_bin`');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
     }
 }
