4cce977f0bff446bbae2cd70654c837c7a4f0de8
Jan Staněk <jan@jstanek.cz>
2022-10-06 23:06:23 +0200

    patrol, troop entities

--- app/Model/User/Patrol.php
+++ app/Model/User/Patrol.php
@@ -1,8 +1,41 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Model\User;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Entita družina.
+ */
+#[ORM\Entity]
+#[ORM\Table(name: 'patrol')]
 class Patrol
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer', nullable: false)]
+    private ?int $id           = null;
 
+    /**
+     * Název družiny.
+     */
+    #[ORM\Column(type: 'string')]
+    protected string $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 }
