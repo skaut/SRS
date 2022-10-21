cfbba51c25cb27f5e9f1b83d3428dcf7fb0795a7
Jan Staněk <jan@jstanek.cz>
2022-10-21 21:58:32 +0200

    db model update, commands

---
+++ app/Model/User/Commands/RegisterTroopGuides.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\Commands;
+
+use App\Model\Program\Program;
+use App\Model\User\User;
+
+class RegisterTroopGuides
+{
+    public function __construct(
+        private User $user,
+        private Program $program,
+        private bool $notifyUser = true
+    ) {
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function getProgram(): Program
+    {
+        return $this->program;
+    }
+
+    public function isNotifyUser(): bool
+    {
+        return $this->notifyUser;
+    }
+}
