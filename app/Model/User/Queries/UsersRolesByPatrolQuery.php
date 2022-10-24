b2ad4e3602cbf2d2c9bf7df2047b51dc87fd6f38
Jan Staněk <jan@jstanek.cz>
2022-10-24 22:05:57 +0200

    2 more steps of form

---
+++ app/Model/User/Queries/UsersRolesByPatrolQuery.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\Queries;
+
+class UsersRolesByPatrolQuery
+{
+    public function __construct(private int $patrolId)
+    {
+    }
+
+    public function getPatrolId(): int
+    {
+        return $this->patrolId;
+    }
+}
