2321adfe75e9d0e5da8e98e13e79f869a5faa69f
Jan Staněk <jan@jstanek.cz>
2022-11-01 19:13:05 +0100

    Admin module prehledy - rebased (#895)
    * skautis personall
    
    * db model update, commands
    
    * forms
    
    * 2 more steps of form
    
    * coding standard
    
    * cleanup, fixes deploment
    
    * deploy from PR
    
    * deploy revert, cs fix
    
    * AdminModule:Users - lokalizace a položky pro Dashboard
    
    * prehled druzin-demo
    
    * prehledy-skupiny-rozdelano
    
    * pridani dalsich sloupcu
    
    nefunguje mazani, detail a export
    
    * Přehledy: jen confirmedPatrols
    
    Skupiny - zobrazuje korektní počet v sloupci počet družin, v Družiny - nezobrazuje řádky s nepotvrzenými družinami vůbec
    
    * rebase fix
    
    * phpstan fix
    
    * phpstan + cs fix
    
    Co-authored-by: bojovyletoun <>

--- app/AdminModule/Presenters/UsersPresenter.php
+++ app/AdminModule/Presenters/UsersPresenter.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace App\AdminModule\Presenters;
 
 use App\AdminModule\Components\ApplicationsGridControl;
+use App\AdminModule\Components\GroupsGridControl;
 use App\AdminModule\Components\IApplicationsGridControlFactory;
+use App\AdminModule\Components\IGroupsGridControlFactory;
+use App\AdminModule\Components\IPatrolsGridControlFactory;
 use App\AdminModule\Components\IUsersGridControlFactory;
+use App\AdminModule\Components\PatrolsGridControl;
 use App\AdminModule\Components\UsersGridControl;
 use App\AdminModule\Forms\AddLectorFormFactory;
 use App\AdminModule\Forms\EditUserPersonalDetailsFormFactory;
@@ -36,6 +40,12 @@ class UsersPresenter extends AdminBasePresenter
 
     #[Inject]
     public IUsersGridControlFactory $usersGridControlFactory;
+
+    #[Inject]
+    public IPatrolsGridControlFactory $patrolsGridControlFactory;
+
+    #[Inject]
+    public IGroupsGridControlFactory $GroupsGridControlFactory;
 
     #[Inject]
     public AddLectorFormFactory $addLectorFormFactory;
@@ -177,6 +187,16 @@ class UsersPresenter extends AdminBasePresenter
     protected function createComponentUsersGrid(): UsersGridControl
     {
         return $this->usersGridControlFactory->create();
+    }
+
+    protected function createComponentPatrolsGrid(): PatrolsGridControl
+    {
+        return $this->patrolsGridControlFactory->create();
+    }
+
+    protected function createComponentGroupsGrid(): GroupsGridControl
+    {
+        return $this->GroupsGridControlFactory->create();
     }
 
     protected function createComponentAddLectorForm(): Form
