f57175130f82650ae1f5807ef108121afcb003c5
bojovyletoun <>
2022-11-10 16:38:35 +0100

    datetime->date
    windows problem s znakem :

--- app/AdminModule/Components/GroupsGridControl.php
+++ app/AdminModule/Components/GroupsGridControl.php
@@ -67,9 +67,9 @@ class GroupsGridControl extends Control
         $grid->setItemsPerPageList([25, 50, 100, 250, 500]);
         $grid->setStrictSessionFilterValues(false);
 
-        $stamp = date(Helpers::DATETIME_FORMAT);
-        $grid->addExportCsv('admin.common.export_all', 'NSJ2023 Skupiny ' . $stamp . 'csv');
-        $grid->addExportCsvFiltered('admin.common.export_filter', 'NSJ2023 Skupiny fi ' . $stamp . 'csv');
+        $stamp = date(Helpers::DATE_FORMAT);
+        $grid->addExportCsv('admin.common.export_all', 'NSJ2023 Skupiny ' . $stamp . '.csv');
+        $grid->addExportCsvFiltered('admin.common.export_filter', 'NSJ2023 Skupiny fi ' . $stamp . '.csv');
 
         $grid->addGroupAction('Export seznamu skupin')
             ->onSelect[] = [$this, 'groupExportUsers'];
