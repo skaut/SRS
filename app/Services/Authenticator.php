e1d0f41ebedc8460f53a1512dc5e48b46614105d
Jan Staněk <jan@jstanek.cz>
2022-12-16 02:55:14 +0100

    cs fix

--- app/Services/Authenticator.php
+++ app/Services/Authenticator.php
@@ -54,7 +54,7 @@ class Authenticator implements Nette\Security\Authenticator
         $firstLogin = false;
         if ($user === null) {
             // nacten ze skautIS pres skupinu
-            $user = $this->userRepository->findBySkautISPersonId($skautISUser->ID_Person) ?? new User();
+            $user              = $this->userRepository->findBySkautISPersonId($skautISUser->ID_Person) ?? new User();
             $roleNonregistered = $this->roleRepository->findBySystemName(Role::NONREGISTERED);
             $user->addRole($roleNonregistered);
             $firstLogin = true;
