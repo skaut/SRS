aa691d6d8e2a98c76160e2b30a8e259244fc884a
Jan Staněk <jan@jstanek.cz>
2023-01-18 20:17:31 +0100

    troop leader email

--- app/AdminModule/UsersModule/Components/TroopsGridControl.php
+++ app/AdminModule/UsersModule/Components/TroopsGridControl.php
@@ -95,9 +95,14 @@ class TroopsGridControl extends Control
                 return Html::el('a')->setAttribute('href', $this->getPresenter()->link('Users:detail', $leader->getId()))->setText($leader->getDisplayName());
             });
 
+        $grid->addColumnText('leaderEmail', 'E-mail vedoucího')
+            ->setRenderer(static function (Troop $t) {
+                return $t->getLeader()->getEmail();
+            });
+
         $grid->addColumnDateTime('applicationDate', 'Datum založení')
-            ->setRenderer(static function (Troop $p) {
-                $date = $p->getApplicationDate();
+            ->setRenderer(static function (Troop $t) {
+                $date = $t->getApplicationDate();
 
                 return $date ? $date->format(Helpers::DATETIME_FORMAT) : '';
             })
