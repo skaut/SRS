180a03de0598145540edfaa835cb71ef0d48adeb
Jan Staněk <jan@jstanek.cz>
2023-04-05 22:48:25 +0200

    phone fix

--- app/AdminModule/Components/UsersGridControl.php
+++ app/AdminModule/Components/UsersGridControl.php
@@ -228,6 +228,9 @@ class UsersGridControl extends Control
             ->setFilterText();
 
         $grid->addColumnText('phone', 'admin.users.users_phone')
+            ->setRenderer(static fn (User $row) => Html::el('a')
+                ->href('tel:' . $row->getPhone())
+                ->setText($row->getPhone()))
             ->setFilterText();
 
         $grid->addColumnText('city', 'admin.users.users_city')
