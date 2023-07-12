b3a96860a29c9f88be6cefbc0180395a4eb94148
Jan Staněk <jan@jstanek.cz>
2023-07-13 00:14:00 +0200

    update rector config

--- rector.php
+++ rector.php
@@ -2,23 +2,14 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
-use Rector\Doctrine\Set\DoctrineSetList;
-use Rector\Nette\Set\NetteSetList;
+use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
-    $containerConfigurator->import(SetList::PHP_80);
-    $containerConfigurator->import(DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES);
-    $containerConfigurator->import(NetteSetList::ANNOTATIONS_TO_ATTRIBUTES,);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PATHS, [
-        __DIR__ . '/src'
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_81,
+        SetList::PHP_81,
+        SetList::CODE_QUALITY,
     ]);
-
-    // $services = $containerConfigurator->services();
 };
