6f234cc653354fd44cb6f1acaf4031986b415794
Jan Staněk <jan@jstanek.cz>
2023-10-18 20:11:45 +0200

    migration fix

--- migrations/Version20231013144631.php
+++ migrations/Version20231013144631.php
@@ -18,6 +18,7 @@ final class Version20231013144631 extends AbstractMigration
     {
         $this->addSql('CREATE TABLE mail_queue (id INT AUTO_INCREMENT NOT NULL, mail_id INT DEFAULT NULL, recipient_email VARCHAR(255) NOT NULL, recipient_name VARCHAR(255) DEFAULT NULL, sent TINYINT(1) NOT NULL, enqueue_datetime DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', send_datetime DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_4B3EDD0CC8776F01 (mail_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE mail_queue ADD CONSTRAINT FK_4B3EDD0CC8776F01 FOREIGN KEY (mail_id) REFERENCES mail (id)');
+        $this->addSql('ALTER TABLE mail ADD recipient_emails LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\'');
     }
 
     public function down(Schema $schema): void
