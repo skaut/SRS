36683c301df7274cade2f1a139befb791f28eb1f
Jan Staněk <jan@jstanek.cz>
2024-07-11 18:56:20 +0200

    Revert tinyMCE (#1299)
    * Revert "Bump tinymce from 6.8.3 to 7.1.1 (#1262)"
    
    This reverts commit c0101300
    
    * revert to 5.x

--- app/assets/admin/main.js
+++ app/assets/admin/main.js
@@ -7,13 +7,13 @@ import tinymce from 'tinymce';
 import 'tinymce/themes/silver';
 import 'tinymce/skins/ui/oxide/skin.css'
 import 'tinymce-i18n/langs/cs';
-import 'tinymce/models/dom'
 import 'tinymce/icons/default/icons';
 import 'tinymce/plugins/autolink';
 import 'tinymce/plugins/lists';
 import 'tinymce/plugins/link';
 import 'tinymce/plugins/code';
 import 'tinymce/plugins/fullscreen';
+import 'tinymce/plugins/paste';
 import contentUiCss from '!!raw-loader!tinymce/skins/ui/oxide/content.css';
 import contentCss from '!!raw-loader!tinymce/skins/content/default/content.css';
 
@@ -26,12 +26,11 @@ tinymce.init({
     skin: false,
     content_css: false,
     content_style: contentUiCss.toString() + '\n' + contentCss.toString(),
-    plugins: 'autolink lists link code fullscreen',
+    plugins: 'autolink lists link code fullscreen paste',
     toolbar: 'undo redo | bold italic | bullist numlist | link unlink | code | fullscreen',
     paste_auto_cleanup_on_paste: true,
     convert_urls : false,
     relative_urls: false,
-    license_key: 'gpl',
 });
 
 tinymce.init({
@@ -43,13 +42,12 @@ tinymce.init({
     skin: false,
     content_css: false,
     content_style: contentUiCss.toString() + '\n' + contentCss.toString(),
-    plugins: 'autolink lists link code fullscreen',
-    toolbar: 'undo redo | blocks | bold italic | bullist numlist | link unlink | code | fullscreen',
+    plugins: 'autolink lists link code fullscreen paste',
+    toolbar: 'undo redo | formatselect  | bold italic | bullist numlist | link unlink | code | fullscreen',
     paste_auto_cleanup_on_paste: true,
     convert_urls : false,
     relative_urls: false,
     block_formats: 'Paragraph=p;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6;Preformatted=pre',
-    license_key: 'gpl',
 });
 
 // generování slugu
