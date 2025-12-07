81a809c9ff1a68e09a6bf2d1fc448843281a7882
Jan Staněk <jan@jstanek.cz>
2025-12-07 09:54:48 +0100

    disable multiselect

--- app/AdminModule/ConfigurationModule/Forms/CustomInputFormFactory.php
+++ app/AdminModule/ConfigurationModule/Forms/CustomInputFormFactory.php
@@ -170,6 +170,9 @@ class CustomInputFormFactory
     {
         $options = [];
         foreach (CustomInput::$types as $type) {
+            if ($type == CustomInput::MULTISELECT)
+                continue;
+
             $options[$type] = 'admin.common.custom_' . $type;
         }
 
