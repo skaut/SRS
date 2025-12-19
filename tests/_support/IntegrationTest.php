a45eac0ae8c57f22c48e723382e2c06123bf6eec
Jan Staněk <jan@jstanek.cz>
2025-12-19 22:52:27 +0100

    test fix

--- tests/_support/IntegrationTest.php
+++ tests/_support/IntegrationTest.php
@@ -8,8 +8,6 @@ use Doctrine\ORM\Tools\SchemaTool;
 
 abstract class IntegrationTest extends Codeception\Test\Unit
 {
-    protected IntegrationTester $tester;
-
     /** @var ClassMetadata[] */
     private array $metadata;
 
@@ -25,18 +23,12 @@ abstract class IntegrationTest extends Codeception\Test\Unit
         return [];
     }
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->tester = new IntegrationTester();
-    }
-
     /**
      * @throws \Doctrine\ORM\Tools\ToolsException
      */
     protected function _before() : void
     {
-        $this->em         = $this->tester->grabService(EntityManagerInterface::class);
+        $this->em         = $this->getModule('IntegrationTester')->grabService(EntityManagerInterface::class);
         $this->metadata   = array_map([$this->em, 'getClassMetadata'], $this->getTestedEntities());
         $this->schemaTool = new SchemaTool($this->em);
         $this->schemaTool->dropSchema($this->metadata);
