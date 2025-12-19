db3fdaec0969cbced6ec7eac08a3a874b3ec5dea
Jan Staněk <jan@jstanek.cz>
2025-12-19 20:22:55 +0100

    test fix

--- tests/_support/IntegrationTest.php
+++ tests/_support/IntegrationTest.php
@@ -25,12 +25,17 @@ abstract class IntegrationTest extends Codeception\Test\Unit
         return [];
     }
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->tester = new IntegrationTester();
+    }
+
     /**
      * @throws \Doctrine\ORM\Tools\ToolsException
      */
     protected function _before() : void
     {
-        $this->tester     = new IntegrationTester();
         $this->em         = $this->tester->grabService(EntityManagerInterface::class);
         $this->metadata   = array_map([$this->em, 'getClassMetadata'], $this->getTestedEntities());
         $this->schemaTool = new SchemaTool($this->em);
